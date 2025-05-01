1ff95197578ea4badf311807d6011349e922eb81
Mohamed-AboElsaad <mohamed32882016@gmail.com>
2025-05-01 17:20:36 +0300

    3mk

--- src/test/java/Testing/BaseTests.java
+++ src/test/java/Testing/BaseTests.java
@@ -5,16 +5,11 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.*;
 
 import java.awt.*;
+import java.time.Duration;
 
 public class BaseTests{
-    WebDriver driver = new EdgeDriver();
-    BasePage B = new BasePage(driver);
-    LoginPage L = new LoginPage(driver);
-    inventoryPage I = new inventoryPage(driver);
-    CartPage Cart = new CartPage(driver);
-    CheckoutPage Checkout = new CheckoutPage(driver);
-    FinalCheckout Final = new FinalCheckout(driver);
-    ThankyouPage Thankyou = new ThankyouPage(driver);
+ WebDriver driver;
+
  @DataProvider(name = "loginData")
 public Object[][] loginData() throws Exception {
     ExcelReader excelReader = new ExcelReader();
@@ -22,33 +17,12 @@ public Object[][] loginData() throws Exception {
 }
     @BeforeClass
     public void setup() {
-        driver.get("https://www.saucedemo.com/v1/");
-        driver.manage().window().maximize();
+     driver = new EdgeDriver();
+        driver.get("https://www.saucedemo.com");
+//        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
     }
-    @Test()
-    public void EndtoEndTest() {
-        B.wait(5);
-        B.typeText(L.username,"standard_user");
-        B.typePassword(L.password,"secret_sauce");
-        B.clickButton(L.loginButton);
-        B.wait(5);
-        B.clickButton(I.SauceLabsBackpack);
-        B.clickButton(I.SauceLabsFleeceJacket);
-        B.clickButton(I.Cartbutton);
-        B.wait(5);
-        B.verifyUrl(Cart.Cartpagelink);
-        B.clickButton(Cart.Checkoutbutton);
-        B.wait(5);
-        B.verifyUrl(Checkout.Checkoutpagelink);
-        B.typeText(Checkout.FristName,"mohamed");
-        B.typeText(Checkout.LastName,"tal3at");
-        B.typeText(Checkout.PostalCode,"22512");
-        B.clickButton(Checkout.ContinueButton);
-        B.wait(5);
-        B.verifyUrl(Final.FinalCheckoutpagelink);
-        B.clickButton(Final.FinishButton);
-        B.verifyText(Thankyou.ThankYouPath,Thankyou.ThankYouText);
-    }
+
 
     @AfterClass
     public void tearDown() {
--- src/test/java/Testing/testData/PageTest.java
+++ src/test/java/Testing/testData/PageTest.java
@@ -1,4 +1,43 @@
 package Testing.testData;
 
-public class PageTest {
+import Testing.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class PageTest extends BaseTests {
+    WebDriver driver = new EdgeDriver();
+    BasePage B = new BasePage(driver);
+    LoginPage L = new LoginPage(driver);
+    inventoryPage I = new inventoryPage(driver);
+    CartPage Cart = new CartPage(driver);
+    CheckoutPage Checkout = new CheckoutPage(driver);
+    FinalCheckout Final = new FinalCheckout(driver);
+    ThankyouPage Thankyou = new ThankyouPage(driver);
+
+
+    @Test()
+    public void EndtoEndTest() {
+        B.wait(5);
+        B.typeText(L.username,"standard_user");
+        B.typePassword(L.password,"secret_sauce");
+        B.clickButton(L.loginButton);
+        B.wait(5);
+        B.clickButton(I.SauceLabsBackpack);
+        B.clickButton(I.SauceLabsFleeceJacket);
+        B.clickButton(I.Cartbutton);
+        B.wait(5);
+        B.verifyUrl(Cart.Cartpagelink);
+        B.clickButton(Cart.Checkoutbutton);
+        B.wait(5);
+        B.verifyUrl(Checkout.Checkoutpagelink);
+        B.typeText(Checkout.FristName,"mohamed");
+        B.typeText(Checkout.LastName,"tal3at");
+        B.typeText(Checkout.PostalCode,"22512");
+        B.clickButton(Checkout.ContinueButton);
+        B.wait(5);
+        B.verifyUrl(Final.FinalCheckoutpagelink);
+        B.clickButton(Final.FinishButton);
+        B.verifyText(Thankyou.ThankYouPath,Thankyou.ThankYouText);
+    }
 }
